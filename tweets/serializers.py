2ebfef470ae8ad4998b063ef3da3c50b99cc17ae
Osama Al Mazloum <osama.almazloum@myhost.local>
2020-09-16 18:33:50 -0400

    DJ-7

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -5,12 +5,33 @@ from rest_framework import serializers
 MAX_TWEET_LENGTH = settings.MAX_TWEET_LENGTH
 
 
-print(settings.TWEET_ACTION_OPTIONS)
+TWEET_ACTION_OPTIONS =['like', 'unlike', 'retweet']
 
-TWEET_ACTION_OPTIONS =['like', 'unlike', 'delete']
+
+
+
+class TweetCreateSerializer(serializers.ModelSerializer):
+    likes = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = Tweets
+        fields = ['id','content','likes']
+    def get_likes(self,obj):
+        return obj.likes.count()
+    
+    def validate_content(self, value):
+        if len(value) > MAX_TWEET_LENGTH:
+            raise serializers.ValidationError('This Tweet is too long')
+        return value
+
+
+
+
 class TweetActionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     action = serializers.CharField()
+    content = serializers.CharField(allow_blank= True, required=False)
+
 
     def validate_action(self, value):
         value = value.lower().strip()
@@ -22,15 +43,17 @@ class TweetActionSerializer(serializers.Serializer):
 
 class TweetSerializer(serializers.ModelSerializer):
     likes = serializers.SerializerMethodField(read_only=True)
+    content= serializers.SerializerMethodField(read_only=True)
+    parent = TweetCreateSerializer(read_only=True)
+
     class Meta:
         model = Tweets
-        fields = ['id','content','likes']
+        fields = ['id','content','likes', 'is_retweet','parent']
     def get_likes(self,obj):
         return obj.likes.count()
-    
-    def validate_content(self, value):
-        if len(value) > MAX_TWEET_LENGTH:
-            raise serializers.ValidationError('This Tweet is too long')
-        return value
-    
+    def get_content(self,obj):
+        content = obj.content
+        if obj.is_retweet:
+            content = obj.parent.content
+        return content
     
